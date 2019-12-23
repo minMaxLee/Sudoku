1422127da88af59b0c5247d0aa9de63d42713363
Minho Lee <leespamspam@gmail.com>
2019-12-23 13:11:12 +0900

    drawing panel

--- Board.java
+++ Board.java
@@ -1,4 +1,15 @@
+import java.awt.Graphics;
 
 public class Board {
-
+	
+	public static void main(String[] args){
+		DrawingPanel dp = new DrawingPanel(900,900);
+		Graphics g = dp.getGraphics();
+		drawBoard(g);
+	}
+	
+	
+	public static void drawBoard(Graphics g){
+		
+	}
 }
